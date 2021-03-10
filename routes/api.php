7bb84735a03b0f35c9bd45902bb4166daf552fe8
Doryan <doryan.chaigneau@hotmail.fr>
2021-03-10 21:08:48 +0100

    Final

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Front Office
 Route::get('/halls','SalleController@getHallsDetails');
 Route::get('/allBookings/{ligueId}','ReservationController@allBookingsById');
 Route::post('/auth','LigueController@login');
 Route::post('/makeReservation','ReservationController@makeReservation');
 Route::post('/league','LigueController@getLeagueById');
 
-// Back Office
 Route::get('/currentBookingsList','ReservationController@getCurrentBookingsList');
 Route::get('/currentBookingsList/{ligueId}','ReservationController@getCurrentBookingsListById');
 Route::get('/allAccounts','LigueController@getAllAccounts');
@@ -18,6 +16,3 @@ Route::get('/changeHallFeatureById/{salleId}','SalleController@getDescriptionByH
 Route::post('/activation','LigueController@changeActivationStatus');
 Route::put('/changeHallFeature','SalleController@changeHallFeature');
 Route::put('/changeDigicode','SalleController@changeDigicode');
-
-// FO & BO
-Route::post('/settingLeague','LigueController@settingLeague');
